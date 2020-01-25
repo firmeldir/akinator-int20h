72d8dde4e2c4aefe9ec8a764eda593c28a8c82f5
Maxim Sutkovenko <mr.gigant977@gmail.com>
2020-01-25 13:50:08 +0200

    Add playerViewModel using sample in MainActivity.kt

--- app/src/main/java/com/example/akinator/MainActivity.kt
+++ app/src/main/java/com/example/akinator/MainActivity.kt
@@ -16,6 +16,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //playerViewModel
+
+//        playerViewModel.playbackState.observe(this, Observer {
+//            when(it.state) {
+//                STATE_PLAYING -> Log.d(TAG, "on Create: State Playing")
+//                STATE_PAUSED -> Log.d(TAG, "onCreate: STATE_PAUSED")
+//                STATE_STOPPED -> Log.d(TAG, "onCreate: State_STOPPED")
+//                STATE_ERROR -> Log.d(TAG, "onCreate: State_Error")
+//            }
+//        })
     }
 
     override fun onDestroy() {
